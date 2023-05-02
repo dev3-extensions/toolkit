1890e8545652176c268c9afe09cdb3d2ce84ed36
Aryan Prince <aryanprincesanty@icloud.com>
2023-05-02 22:36:07 +0100

    feat: Button now shows pause/play icons

--- src/components/ui/PauseButton.tsx
+++ src/components/ui/PauseButton.tsx
@@ -1,19 +1,19 @@
-import { PauseIcon } from 'lucide-react'
+import { PauseIcon, Play } from 'lucide-react'
 
 type PauseButtonProps = {
+  isPaused: boolean
   setIsPaused: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const PauseButton = ({ setIsPaused }: PauseButtonProps) => {
+export const PauseButton = ({ isPaused, setIsPaused }: PauseButtonProps) => {
   return (
     <button
       onClick={() => {
         setIsPaused((prev) => !prev)
       }}
-      className="flex items-center gap-2 rounded-lg bg-amber-500 px-6 py-3 font-medium text-white hover:bg-amber-600 active:bg-amber-700"
+      className="flex items-center gap-2 rounded-lg bg-neutral-500 p-3 font-medium text-white hover:bg-neutral-600 active:bg-neutral-700"
     >
-      <PauseIcon size={20} />
-      Pause
+      {isPaused ? <Play size={20} /> : <PauseIcon size={20} />}
     </button>
   )
 }
